7f089be4a0379a3d88853eeeff8b25fab6ae706f
xnor <lukacrisb2@gmail.com>
2014-05-25 14:50:36 -0400

    fetching form web works

--- src/com/example/Diablo3/Diablo.java
+++ src/com/example/Diablo3/Diablo.java
@@ -1,15 +1,72 @@
 package com.example.Diablo3;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.widget.TextView;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 
 public class Diablo extends Activity {
     /**
      * Called when the activity is first created.
      */
+
+    private TextView textView;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        textView = (TextView) findViewById(R.id.TextView01);
     }
+
+    private class DownloadWebPageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            for (String url : urls) {
+                DefaultHttpClient client = new DefaultHttpClient();
+                HttpGet httpGet = new HttpGet(url);
+
+                try {
+                    HttpResponse execute = client.execute(httpGet);
+                    InputStream content = execute.getEntity().getContent();
+
+                    BufferedReader br = new BufferedReader(
+                            new InputStreamReader(content)
+                    );
+
+                    String tmp = "";
+
+                    while ((tmp = br.readLine()) != null) {
+                        response += tmp;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            textView.setText(Html.fromHtml(result));
+        }
+    }
+
+    public void readWebpage(View view) {
+        DownloadWebPageTask task = new DownloadWebPageTask();
+        task.execute(new String[] { "http://www.diabloprogress.com/", "http://us.battle.net/api/d3/profile/Itherael-1662/" });
+
+    }
+
 }
